e561b39bf6c1f886f197172d0cb5d23f634ffc63
Jack-Andrew-Robert-Sanderson <jacksanderson123@hotmail.co.uk>
2018-07-19 15:00:22 +0100

    Removed hardcoded layout from the viewDidLoad function, so that this functionality can be demonstrated using story board

--- click counter/ViewController.swift
+++ click counter/ViewController.swift
@@ -16,41 +16,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Set Label
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        //Add View to the view higherarchy
-        view.addSubview(label)
-        self.lblCount = label
-        
-        //Label
-        let labelTwo = UILabel()
-        labelTwo.frame = CGRect(x: 150, y: 200, width: 60, height: 60)
-        labelTwo.text = "0"
-        view.addSubview(labelTwo)
-        self.lblIncrement = labelTwo
-        
-        
-        //Increment Button
-        let button = UIButton()
-        button.frame = CGRect(x: 200, y: 250, width: 100, height: 60)
-        button.setTitle("Increment", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
-        
-        //Decrement Button
-        let btnDecrement = UIButton()
-        btnDecrement.frame = CGRect(x: 50, y: 250, width: 100, height: 60)
-        btnDecrement.setTitle("Decrement", for: .normal)
-        btnDecrement.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(btnDecrement)
-        
-        btnDecrement.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControlEvents.touchUpInside)
-        
     }
     @objc private func decrementCount(){
         self.count -= 1
